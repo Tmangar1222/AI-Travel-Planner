d9540143449705cdf9e1b96c0d6be8340489891f
Taruna Mangar <nmangar123@gmail.com>
2025-02-24 22:04:12 -0500

    budget section

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -38,9 +38,18 @@ function CreateTrip() {
 
       <div>
         <h2 className="text-xl my-3 font-medium">What is your budget?</h2>
-        {/* <div>
-          {SelectBudgetOptions.map((item, index))}
-        </div> */}
+        <div className='grid grid-cols-3 gap-5 mt-5'>
+        {SelectBudgetOptions.map((item, index) => (
+          <div key={index} className='p-4 border rounded-lg hover:shadow-lg'>
+          <h2 className='text-4xl'>{item.icon}</h2>
+          <h2 className='font-bold text-xl'>{item.title}</h2>
+          <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+          <h2 className='text-sm text-gray-500'>{item.range}</h2>
+          
+          </div>
+        ))}
+
+        </div>
       </div>
 
     </div>
